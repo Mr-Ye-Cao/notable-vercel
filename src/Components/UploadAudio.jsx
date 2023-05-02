f625960dfa01152d0216198eb735d51ac71676d2
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-05-02 14:33:30 -0500

    resolve conflict

--- src/Components/UploadAudio.jsx
+++ src/Components/UploadAudio.jsx
@@ -17,23 +17,23 @@ export default function UploadAudio() {
     }
 
     const fetchData = async (file) => {
-      const formData = new FormData();
-      formData.append("audio", file);
-    
-      try {
-        const response = await fetch("/api/processAudio", {
-          method: "POST",
-          body: formData,
-        });
-        const data = await response.json();
-        console.log(data.message);
-        console.log(data.htmlNote);
-        
-        setHtmlNote(data.htmlNote);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
+        const formData = new FormData();
+        formData.append("audio", file);
+      
+        try {
+          const response = await fetch("/api/processAudio", {
+            method: "POST",
+            body: formData,
+          });
+          const data = await response.json();
+          console.log(data.message);
+          console.log(data.htmlNote);
+          
+          setHtmlNote(data.htmlNote);
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
 
     function handleSubmission(e){
         e.preventDefault();
@@ -46,26 +46,57 @@ export default function UploadAudio() {
     }
 
   return (
-
-    <div className='d-flex flex-column justify-content-center align-items-center'>
-        <button className='btn' onClick={(e)=>handleFileUpload(e)}>
-            <i className="bi bi-upload fs-1 dashline-btn p-3 px-4">
-            </i>
-        </button>
-        <input className="form-control" type="file" id="formFile" ref={hiddenFileInput} 
-        accept=".mp3,.wav,.m4a"
-        onChange={handleFileChange} style={{display: "none"}}/>
+    <>
         {
-            isFilePicked ?
-            <div className='d-flex flex-column justify-content-center align-items-center mt-5'>
-                <div>
-					<p>{selectedFile.name}</p>
-				</div>
-                <button className='btn btn-primary' onClick={handleSubmission}>SUBMIT</button>
-            </div>
-            :
-            <></>
+            htmlNote 
+                ?
+
+                <iframe style={{ width: "100%", height: "100%" }} srcDoc={htmlNote}/>
+                // <iframe style={{ width: "100%", height: "100%" }} srcDoc={`<!DOCTYPE html>
+                // <html>
+                //   <head>
+                //     <title>Sample HTML Page</title>
+                //   </head>
+                //   <body>
+                //     <h1>Welcome to my Sample HTML Page</h1>
+                //     <p>Here's some content for this page:</p>
+                //     <ul>
+                //       <li>Item 1</li>
+                //       <li>Item 2</li>
+                //       <li>Item 3</li>
+                //     </ul>
+                //     <p>This is a paragraph with some text.</p>
+                //     <p>And this is another paragraph with some more text.</p>
+                //   </body>
+                // </html>
+                // `}></iframe>
+
+                :
+            (
+
+                <div className='d-flex flex-column justify-content-center align-items-center'>
+                    <button className='btn' onClick={(e)=>handleFileUpload(e)}>
+                        <i className="bi bi-upload fs-1 dashline-btn p-3 px-4">
+                        </i>
+                    </button>
+                    <input className="form-control" type="file" id="formFile" ref={hiddenFileInput} 
+                    accept=".mp3,.wav,.m4a"
+                    onChange={handleFileChange} style={{display: "none"}}/>
+                    {
+                        isFilePicked ?
+                        <div className='d-flex flex-column justify-content-center align-items-center mt-5'>
+                            <div>
+                                <p>{selectedFile.name}</p>
+                            </div>
+                            <button className='btn btn-primary' onClick={handleSubmission}>SUBMIT</button>
+                        </div>
+                        :
+                        <></>
+                    }
+                </div>
+            )
         }
-    </div>
+    </>
+
   )
 }
